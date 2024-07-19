f93eb7530cacb58319cf28e5f1fee4c16f1b6555
akhilakailam123 <78717105+akhilakailam123@users.noreply.github.com>
2024-07-19 12:17:54 +0530

    Update sample_program.py

--- sample_program.py
+++ sample_program.py
@@ -2,5 +2,10 @@ def add(a, b):
     return a + b
 
 
-assert add(30, 4) == 34
-assert add(2, 67) == 69
+def test_add():
+    assert add(30, 4) == 34
+    assert add(2, 67) == 69
+
+
+if __name__ == "__main__":
+    test_add()
