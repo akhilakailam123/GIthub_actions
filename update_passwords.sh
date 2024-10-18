149d0197779a1461f983acab2b0410a741801966
akhilakailam123 <78717105+akhilakailam123@users.noreply.github.com>
2024-10-18 15:40:03 +0530

    Update update_passwords.sh

--- update_passwords.sh
+++ update_passwords.sh
@@ -25,8 +25,10 @@ for server in "${!server_users[@]}"; do
   # Iterate over each user in the current server
   for username in $users; do
     new_password="${passwords[$username]}"
-    echo $new_password
     
+    # Debugging: Print the password for the user
+    echo "Password for $username: $new_password"
+
     if [ -n "$new_password" ]; then
       echo "Processing password for $username on $server..."
 
